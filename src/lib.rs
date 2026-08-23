1892bc56de0b931b53dbdde21c367ad96f4532bb
Jon Heinritz <jon.heinritz@protonmail.com>
2026-08-23 05:04:05 +0200

    feat: triangle! Now also forwards!

--- src/lib.rs
+++ src/lib.rs
@@ -130,7 +130,7 @@ impl State {
             primitive: wgpu::PrimitiveState {
                 topology: wgpu::PrimitiveTopology::TriangleList,
                 strip_index_format: None,
-                front_face: wgpu::FrontFace::Cw,
+                front_face: wgpu::FrontFace::Ccw,
                 cull_mode: Some(wgpu::Face::Back),
                 polygon_mode: wgpu::PolygonMode::Fill,
                 unclipped_depth: false,

9649a9b639d71d3a5654a1a6c481b30d03b8f213
Jon Heinritz <jon.heinritz@protonmail.com>
2026-09-03 09:46:37 +0200

    refactor: put App and State into their own file

---
+++ src/app.rs
@@ -0,0 +1,477 @@
+use crate::{
+    Vertex, color,
+    polygon::{self, CornerUpdateMode},
+};
+use std::{cmp, f32::consts::TAU, sync::Arc};
+use wgpu::BufferUsages;
+use winit::{
+    application::ApplicationHandler,
+    event::*,
+    event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::{Key, NamedKey},
+    window::Window,
+};
+
+const SIDES: usize = 100;
+type PolygonConfig = polygon::PolygonConfig<SIDES, { polygon::index_count(SIDES) }>;
+const MAX_POLYGON_CONFIG: PolygonConfig = PolygonConfig::new(SIDES as u16);
+
+const RED: [f32; 3] = color::rgb_to_f32x3(0xFF0F00);
+const MAGENTA: [f32; 3] = color::rgb_to_f32x3(0xBC00BC);
+
+// This will store the state of our game
+pub struct State {
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
+
+    render_pipeline: wgpu::RenderPipeline,
+    polygon_config: PolygonConfig,
+
+    vertex_buf: &'static mut [Vertex; MAX_POLYGON_CONFIG.max_corners()],
+    index_buf: &'static mut [u16; MAX_POLYGON_CONFIG.max_indeces()],
+
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+
+    window: Arc<Window>,
+}
+
+impl State {
+    // We don't need this to be async right now,
+    // but we will in the next tutorial
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            #[cfg(not(target_arch = "wasm32"))]
+            backends: wgpu::Backends::PRIMARY,
+            #[cfg(target_arch = "wasm32")]
+            backends: wgpu::Backends::GL,
+            flags: Default::default(),
+            memory_budget_thresholds: Default::default(),
+            backend_options: Default::default(),
+            display: None,
+        });
+
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+                apply_limit_buckets: true,
+            })
+            .await?;
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                experimental_features: wgpu::ExperimentalFeatures::disabled(),
+                // WebGL doesn't support all of wgpu's features, so if
+                // we're building for the web we'll have to disable some.
+                required_limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                memory_hints: Default::default(),
+                trace: wgpu::Trace::Off,
+            })
+            .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+        // one will result in all the colors coming out darker. If you want to support non
+        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+            color_space: wgpu::SurfaceColorSpace::Auto,
+        };
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        let polygon_config = PolygonConfig::new(3);
+
+        let vertex_buf = Box::leak(Box::new(
+            [Vertex {
+                position: [0.; _],
+                color: MAGENTA,
+            }; _],
+        ));
+        let index_buf = Box::leak(Box::new([0; _]));
+        let vertex_buffer = device.create_buffer(&wgpu::wgt::BufferDescriptor {
+            label: Some("Vertex Buffer"),
+            size: MAX_POLYGON_CONFIG.vertex_count_bytes(),
+            usage: BufferUsages::VERTEX | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let index_buffer = device.create_buffer(&wgpu::wgt::BufferDescriptor {
+            label: Some("Index Buffer"),
+            size: MAX_POLYGON_CONFIG.index_count_bytes(),
+            usage: BufferUsages::INDEX | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                immediate_size: 0,
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                buffers: &[Some(Vertex::DESC)],
+                compilation_options: Default::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options: Default::default(),
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview_mask: None,
+            cache: None,
+        });
+
+        Ok(Self {
+            surface,
+            device,
+            queue,
+            config,
+            is_surface_configured: false,
+
+            render_pipeline,
+
+            vertex_buffer,
+            index_buffer,
+
+            polygon_config,
+            vertex_buf,
+            index_buf,
+
+            window,
+        })
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        // We'll do stuff here in the next tutorial
+        if width > 0 && height > 0 {
+            self.config.width = width;
+            self.config.height = height;
+            self.surface.configure(&self.device, &self.config);
+            self.is_surface_configured = true;
+        }
+    }
+
+    fn render(&mut self) -> anyhow::Result<()> {
+        self.window.request_redraw();
+
+        // We can't render unless the surface is configured
+        if !self.is_surface_configured {
+            return Ok(());
+        }
+
+        let output = match self.surface.get_current_texture() {
+            wgpu::CurrentSurfaceTexture::Success(surface_texture) => surface_texture,
+            wgpu::CurrentSurfaceTexture::Suboptimal(surface_texture) => surface_texture,
+            wgpu::CurrentSurfaceTexture::Timeout
+            | wgpu::CurrentSurfaceTexture::Occluded
+            | wgpu::CurrentSurfaceTexture::Validation => {
+                // Skip this frame
+                return Ok(());
+            }
+            wgpu::CurrentSurfaceTexture::Outdated => {
+                self.surface.configure(&self.device, &self.config);
+                return Ok(());
+            }
+            wgpu::CurrentSurfaceTexture::Lost => {
+                // You could recreate the devices and all resources
+                // created with it here, but we'll just bail
+                anyhow::bail!("Lost device");
+            }
+        };
+
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        self.vertex_buf[1].color = match self.vertex_buf[0].color {
+            RED => self.polygon_config.mode_color(),
+            MAGENTA => MAGENTA,
+            _ => unreachable!(),
+        };
+        self.polygon_config.write_vertices(self.vertex_buf);
+        self.queue.write_buffer(
+            &self.vertex_buffer,
+            0,
+            bytemuck::cast_slice(self.vertex_buf),
+        );
+        self.polygon_config.write_indeces_ccw(self.index_buf);
+        self.queue
+            .write_buffer(&self.index_buffer, 0, bytemuck::cast_slice(self.index_buf));
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    depth_slice: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(color::rgb_to_wgpu_color(0x20201d)),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+                multiview_mask: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(
+                0,
+                self.vertex_buffer
+                    .slice(..self.polygon_config.vertex_count_bytes()),
+            );
+            render_pass.set_index_buffer(
+                self.index_buffer
+                    .slice(..self.polygon_config.index_count_bytes()),
+                wgpu::IndexFormat::Uint16,
+            );
+            render_pass.draw_indexed(0..self.polygon_config.index_count(), 0, 0..1);
+        }
+
+        // submit will accept anything that implements IntoIter
+        self.queue.submit(std::iter::once(encoder.finish()));
+        self.queue.present(output);
+
+        Ok(())
+    }
+
+    fn update(&mut self) {}
+
+    #[allow(clippy::identity_op)]
+    fn handle_key(&mut self, event_loop: &ActiveEventLoop, key: Key, is_pressed: bool) {
+        match (key.as_ref(), is_pressed) {
+            (Key::Named(NamedKey::Escape), true) => event_loop.exit(),
+            (Key::Named(NamedKey::Space), true) => {
+                let color = &mut self.vertex_buf[0].color;
+                match *color {
+                    RED => *color = MAGENTA,
+                    MAGENTA => *color = RED,
+                    _ => {}
+                }
+            }
+            (Key::Character(c), true) => match c {
+                "+" | "-"
+                    if let CornerUpdateMode::Rotation { rotation } =
+                        self.polygon_config.get_corner_mode_mut() =>
+                {
+                    match c {
+                        "-" => *rotation -= 0.05,
+                        "+" => *rotation += 0.05,
+                        _ => unreachable!(),
+                    }
+                }
+                "+" | "-"
+                    if let CornerUpdateMode::MultiSource { sources } =
+                        self.polygon_config.get_corner_mode_mut() =>
+                {
+                    match c {
+                        "-" => *sources = cmp::max(*sources - 1, 2),
+                        "+" => *sources = cmp::min(*sources + 1, 20),
+                        _ => unreachable!(),
+                    }
+                }
+                "0" => self.polygon_config.set_angle(0.),
+                "h" => self.polygon_config.update_angle(0. - 5. / 360. * TAU),
+                "l" => self.polygon_config.update_angle(0. + 5. / 360. * TAU),
+
+                "k" => self.polygon_config.update_corners(0 + 1),
+                "j" => self.polygon_config.update_corners(0 - 1),
+
+                "n" => self.polygon_config.update_next_corner_mode(),
+
+                _ => {}
+            },
+
+            _ => {}
+        }
+    }
+}
+
+pub struct App {
+    #[cfg(target_arch = "wasm32")]
+    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
+    state: Option<State>,
+}
+
+impl App {
+    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
+        #[cfg(target_arch = "wasm32")]
+        let proxy = Some(event_loop.create_proxy());
+        Self {
+            state: None,
+            #[cfg(target_arch = "wasm32")]
+            proxy,
+        }
+    }
+}
+
+impl ApplicationHandler<State> for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        #[allow(unused_mut)]
+        let mut window_attributes = Window::default_attributes();
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            use wasm_bindgen::{JsCast, UnwrapThrowExt};
+            use winit::platform::web::WindowAttributesExtWebSys;
+
+            const CANVAS_ID: &str = "canvas";
+
+            let window = wgpu::web_sys::window().unwrap_throw();
+            let document = window.document().unwrap_throw();
+            let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw();
+            let html_canvas_element = canvas.unchecked_into();
+            window_attributes = window_attributes.with_canvas(Some(html_canvas_element));
+        }
+
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            // If we are not on web we can use pollster to
+            // await the window creation
+            self.state = Some(pollster::block_on(State::new(window)).unwrap());
+        }
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            // Run the future asynchronously and use the
+            // proxy to send the results to the event loop
+            if let Some(proxy) = self.proxy.take() {
+                wasm_bindgen_futures::spawn_local(async move {
+                    assert!(
+                        proxy
+                            .send_event(
+                                State::new(window)
+                                    .await
+                                    .expect("Unable to create canvas!!!")
+                            )
+                            .is_ok()
+                    )
+                });
+            }
+        }
+    }
+
+    #[allow(unused_mut)]
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
+        // This is where proxy.send_event() ends up
+        #[cfg(target_arch = "wasm32")]
+        {
+            event.window.request_redraw();
+            event.resize(
+                event.window.inner_size().width,
+                event.window.inner_size().height,
+            );
+        }
+        self.state = Some(event);
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: WindowEvent,
+    ) {
+        let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => state.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => {
+                state.update();
+                match state.render() {
+                    Ok(_) => {}
+                    Err(e) => {
+                        // Log the error and exit gracefully
+                        log::error!("{e}");
+                        event_loop.exit();
+                    }
+                }
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        logical_key: key,
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => state.handle_key(event_loop, key, key_state.is_pressed()),
+            _ => {}
+        }
+    }
+}
